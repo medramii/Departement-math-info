702ed1f7b1281cf1f25f22da27dd9aa746af7b8a
Mohamed OUFRAd <oufrad00@gmail.com>
2020-06-06 16:20:38 +0100

    Adding on About page

---
+++ Web/Pages/About/about.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="about.css">
+  </head>
+  <body>
+        <div class="card mb-3 container" style="max-width: 1000px;">
+      <div class="row no-gutters">
+        <div class="col-md-4">
+          <img src="lelouch.jpg" class="card-img" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">About</h5>
+            <p class="card-text">Lelouch vi Britannia (ルルーシュ・ヴィ・ブリタニア, Rurūshu vi Buritania), whose alias is Lelouch Lamperouge (ルルーシュ・ランペルージ, Rurūshu Ranperūji), is the title character and protagonist of the Sunrise anime series Code Geass: Lelouch of the Rebellion. In the series, Lelouch is a former prince from the superpower Britannia who is given the power of the "Geass" by a witch known as C.C. Using the Geass and his genius-level intellect, Lelouch becomes the leader of the resistance movement known as The Black Knights under his alter ego Zero (ゼロ) to destroy the Holy Britannian Empire, an imperial monarchy that has been conquering various countries under control from his father. He is voiced by Jun Fukuyama, and his child self is voiced by Sayaka Ohara.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </body>
+</html>
