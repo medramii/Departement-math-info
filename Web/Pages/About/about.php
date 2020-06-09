e688d6728b66b0005101c58a73d2ebe104c903c1
Mohamed OUFRAd <oufrad00@gmail.com>
2020-06-09 17:33:49 +0100

    brztona bhad about hahya hskna 3liha safi hadchi libito hhhh

--- Web/Pages/About/about.php
+++ Web/Pages/About/about.php
@@ -2,24 +2,76 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="about.css">
+    <title></title>
   </head>
   <body>
-        <div class="card mb-3 container" style="max-width: 1000px;">
-      <div class="row no-gutters">
-        <div class="col-md-4">
-          <img src="lelouch.jpg" class="card-img" alt="...">
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h5 class="card-title">About</h5>
-            <p class="card-text">Lelouch vi Britannia (ルルーシュ・ヴィ・ブリタニア, Rurūshu vi Buritania), whose alias is Lelouch Lamperouge (ルルーシュ・ランペルージ, Rurūshu Ranperūji), is the title character and protagonist of the Sunrise anime series Code Geass: Lelouch of the Rebellion. In the series, Lelouch is a former prince from the superpower Britannia who is given the power of the "Geass" by a witch known as C.C. Using the Geass and his genius-level intellect, Lelouch becomes the leader of the resistance movement known as The Black Knights under his alter ego Zero (ゼロ) to destroy the Holy Britannian Empire, an imperial monarchy that has been conquering various countries under control from his father. He is voiced by Jun Fukuyama, and his child self is voiced by Sayaka Ohara.</p>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+
+
+        <div class="carousel-inner">
+
+          <!-- The First slide (le doyen slide contient l'image et le nome est les information de doyen) -->
+
+          <div class="carousel-item active">
+            <div class="card mb-3 container" style="max-width: 1000px;">
+                <div class="row no-gutters">
+                    <div class="col-md-4">
+                      <img src="lelouch.jpg" class="card-img" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                      <div class="card-body">
+                        <h5 class="card-title">Le doyen</h5>
+                        <p class="card-text">Lelouch vi Britannia (ルルーシュ・ヴィ・ブリタニア, Rurūshu vi Buritania), whose alias is Lelouch Lamperouge (ルルーシュ・ランペルージ, Rurūshu Ranperūji), is the title character and protagonist of the Sunrise anime series Code Geass: Lelouch of the Rebellion. In the series, Lelouch is a former prince from the superpower Britannia who is given the power of the "Geass" by a witch known as C.C. Using the Geass and his genius-level intellect, Lelouch
+                           becomes the leader of the resistance movement known as The Black Knights under his alter ego Zero (ゼロ) to destroy the Holy Britannian Empire, an imperial monarchy that has been conquering various countries under control from his father. He is voiced by Jun Fukuyama, and his child self is voiced by Sayaka Ohara.
+                        </p>
+                      </div>
+                    </div>
+                </div>
+            </div>
+          </div>
+
+          <!-- The Secend slide (fpt slide contient les information de faculté) -->
+
+          <div class="carousel-item">
+            <div class="card mb-3 container" style="max-width: 1000px;">
+                <div class="row no-gutters">
+                    <div class="col-md-8">
+                      <div class="card-body">
+                        <h5 class="card-title">fpt</h5>
+                        <p class="card-text fpt-info">Lelouch vi Britannia (ルルーシュ・ヴィ・ブリタニア, Rurūshu vi Buritania), whose alias is Lelouch Lamperouge (ルルーシュ・ランペルージ, Rurūshu Ranperūji), is the title character and protagonist of the Sunrise anime series Code Geass: Lelouch of the Rebellion. In the series, Lelouch is a former prince from the superpower Britannia who is given the power of the "Geass" by a witch known as C.C. Using the Geass and his genius-level intellect, Lelouch becomes the leader of the resistance movement known as The Black Knights under his alter ego Zero (ゼロ) to destroy the Holy Britannian Empire, an imperial monarchy that has been conquering various countries under control from his father. He is voiced by Jun Fukuyama, and his child self is voiced by Sayaka Ohara.</p>
+                      </div>
+                    </div>
+                </div>
+          </div>
+          </div>
+
+          <!-- The third slide (map slide that show the faculté location) -->
+
+          <div class="carousel-item">
+            <div class="card mb-3 container map-container">
+                  <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3437.8844191227845!2d-8.871443685544811!3d30.496021304054707!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xdb17364c0b9166b%3A0xf4d0e2d7847e8c26!2sFacult%C3%A9%20Polydisciplinaire%20de%20Taroudant!5e0!3m2!1sen!2sma!4v1591719203651!5m2!1sen!2sma" width="975" height="350" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
   </body>
 </html>
