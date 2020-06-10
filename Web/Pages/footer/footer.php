d084bda9404a1ba4a201a9c81eeb67cb813da9c0
Samadi-AA <66368620+Samadi-AA@users.noreply.github.com>
2020-06-10 14:08:08 +0000

    add footer page

---
+++ Web/Pages/footer/footer.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>footer</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="footer.css">
+    <link rel="stylesheet" href="..\..\fontawesome\css\all.css">
+</head>
+<body>
+    <footer>
+        <div class="left-box">
+            <a href="#navbar"><i class="fas fa-chevron-circle-up"></i></a>
+            <img src="logo.png">
+            <img src="fpt.png">
+            <div class="social-media">
+            <a href="https://twitter.com" target="blank"> <i class="fab fa-twitter"></i> </a>
+                <a href="https://facebook.com/siteFPT/" target="blank"> <i class="fab fa-facebook-f"></i> </a>
+                <a href="https://www.youtube.com/channel/UCJkXdGXw4vGU7QhHNa61OTA" target="blank"> <i class="fab fa-youtube"></i> </a>
+                <a href="https://aboutme.google.com" target="blank"> <i class="fab fa-google-plus-g"></i> </a>
+            </div>
+        </div>
+        <div class="right-box">
+            <h2> CONTACTEZ NOUS </h2>
+            <hr color="#0092ca" width="50">
+            <div class="local">
+                <i class="fas fa-map-marker-alt"></i>
+                <span> faculté polydisciplinaire - Hay El Mouhammadi - (Lastah) - B.P: 207 - 83000 Taroudant, Maroc</span>
+            </div>
+            <div class="phone">
+                <i class="fas fa-phone-alt"></i>
+                <span>+212 (0) 528 551 010</span>
+            </div>
+            <div class="fax">
+                <i class="fas fa-fax"></i>
+                <span>+212 (0) 528 551 020</span>
+            </div>
+            <div class="web-site">
+                <i class="fas fa-globe"></i>
+                <span>www.dmi.ac.ma</span>
+            </div>
+        </div>
+        <div class="bottom-box">
+            <h3>Departement math infotmatique  | © 2020 All Rights Reserved</h3>
+        </div>
+    </footer>
+</body>
+</html>
