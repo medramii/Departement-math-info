dd9e0e63a41a1f3a5319eb882f21f3a241a75ca4
unknown <Younes-Fer>
2020-06-10 21:28:39 +0100

    update contact

--- Web/Pages/Contact/contact.php
+++ Web/Pages/Contact/contact.php
@@ -15,33 +15,38 @@
 <div class="container">
  <div  >
 	<form>
-		
-		<h1 >  Contact Us </h1> 
+
+		<h1 >  Contact Us </h1>
 	      <hr class="hr">
 		 	<input type="text" placeholder="Your Name" name="name" class="cls">
-     <br>   <br> <br> 
+     <br>   <br> <br>
 		 	<input type="Email"placeholder="Your Email"  name="email" class="cls">
-     <br>   <br> <br>  
+     <br>   <br> <br>
 		 	<input type="tel"placeholder="Your phone"  name="phone" class="cls">
-     <br>   <br>   <br> 
+     <br>   <br>   <br>
              <textarea  placeholder="Message" class="cl" ></textarea>
-     <br>   <br> <br>   
+     <br>   <br> <br>
               <input type="submit" value="Send" name="Send" class="send">
 
-	 </form> 
+	 </form>
 </div>
 
 
 <div  class="div">
-    <span class="b"><b>Site &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp :&nbsp&nbsp</b> <a href=""> www.fpt_departement.com</a></span>
-    <br class="hr">
-	<span class="b"><b>Tel &nbsp &nbsp &nbsp &nbsp&nbsp &nbsp &nbsp :&nbsp &nbsp</b>+212621345768</span>
+
+	   <span class="b"><i class="fa fa-map-marker" aria-hidden="true" style='font-size:15px'></i><b>&nbsp &nbsp Adress &nbsp&nbsp&nbsp &nbsp:&nbsp&nbsp &nbsp</b> Hay El Mohammadi (Lastah)</span>
+   	<br>
+
+	<span class="b"><i class="fa fa-phone" style='font-size:15px'></i><b> Tel &nbsp &nbsp &nbsp &nbsp &nbsp&nbsp &nbsp &nbsp :&nbsp &nbsp</b>+212 5 28 55 10 10</span>
 	<br>
-	<span class="b"><b>Fax &nbsp &nbsp &nbsp &nbsp&nbsp &nbsp&nbsp :&nbsp &nbsp</b>0521368596</span>
+	<span class="b"><i class="fa fa-fax" aria-hidden="true" style='font-size:15px'></i><b>&nbspFax &nbsp &nbsp &nbsp &nbsp&nbsp &nbsp&nbsp :&nbsp &nbsp</b>+212 5 28 55 10 20</span>
 	<br>
-	<span class="b"><b>Email &nbsp &nbsp &nbsp &nbsp&nbsp :&nbsp &nbsp </b>departement_fpt@gmail.com</span>
+	<span class="b"><i class="fa fa-envelope" aria-hidden="true" style='font-size:15px'></i><b>&nbspEmail &nbsp &nbsp &nbsp &nbsp&nbsp :&nbsp &nbsp </b>departement_fpt@gmail.com</span>
 	<br>
-	<span class="b"><b>Fecebook &nbsp:&nbsp &nbsp</b><i class="fa fa-facebook-square" style="font-size:20px;color:blue"></i>&nbsp Antari Jelali</span>
+	<span class="b"><i class="fa fa-facebook-square" style="font-size:15px;"></i><b>&nbspFecebook &nbsp:&nbsp &nbsp</b>&nbsp Antari Jelali</span>
+<br>
+<span class="b"><b>Site &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp :&nbsp&nbsp &nbsp </b> <a href=""> www.fpt_departement.com</a></span>
+
 </div>
 
 
