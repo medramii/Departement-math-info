2d0ff12e6316ac0029f2d85bf5fe3c1f884317d3
Younes-Fer <61284835+Younes-Fer@users.noreply.github.com>
2020-06-10 20:39:50 +0100

    contact

---
+++ Web/Pages/contact.php
@@ -0,0 +1,58 @@
+<?php  ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" >
+	<title>index</title>
+	<link rel="stylesheet" type="text/css" href="contact.css">
+	 <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+<div class="container">
+ <div  >
+	<form>
+
+		<h1 >  Contact Us </h1>
+	      <hr class="hr">
+		 	<input type="text" placeholder="Your Name" name="name" class="cls">
+     <br>   <br> <br>
+		 	<input type="Email"placeholder="Your Email"  name="email" class="cls">
+     <br>   <br> <br>
+		 	<input type="tel"placeholder="Your phone"  name="phone" class="cls">
+     <br>   <br>   <br>
+             <textarea  placeholder="Message" class="cl" ></textarea>
+     <br>   <br> <br>
+              <input type="submit" value="Send" name="Send" class="send">
+
+	 </form>
+</div>
+
+
+<div  class="div">
+
+	   <span class="b"><i class="fa fa-map-marker" aria-hidden="true" style='font-size:15px'></i><b>&nbsp &nbsp Adress &nbsp&nbsp&nbsp &nbsp:&nbsp&nbsp &nbsp</b> Hay El Mohammadi (Lastah)</span>
+   	<br>
+
+	<span class="b"><i class="fa fa-phone" style='font-size:15px'></i><b> Tel &nbsp &nbsp &nbsp &nbsp &nbsp&nbsp &nbsp &nbsp :&nbsp &nbsp</b>+212 5 28 55 10 10</span>
+	<br>
+	<span class="b"><i class="fa fa-fax" aria-hidden="true" style='font-size:15px'></i><b>&nbspFax &nbsp &nbsp &nbsp &nbsp&nbsp &nbsp&nbsp :&nbsp &nbsp</b>+212 5 28 55 10 20</span>
+	<br>
+	<span class="b"><i class="fa fa-envelope" aria-hidden="true" style='font-size:15px'></i><b>&nbspEmail &nbsp &nbsp &nbsp &nbsp&nbsp :&nbsp &nbsp </b>departement_fpt@gmail.com</span>
+	<br>
+	<span class="b"><i class="fa fa-facebook-square" style="font-size:15px;"></i><b>&nbspFecebook &nbsp:&nbsp &nbsp</b>&nbsp Antari Jelali</span>
+<br>
+<span class="b"><b>Site &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp :&nbsp&nbsp &nbsp </b> <a href=""> www.fpt_departement.com</a></span>
+
+</div>
+
+
+</div>
+
+
+
+</body>
+</html>
