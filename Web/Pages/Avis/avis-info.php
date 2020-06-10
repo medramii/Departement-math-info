5ed1f2471cfdeef40bc7b4ba01a080b82aca1a7a
Mohamed OUFRAd <oufrad00@gmail.com>
2020-06-10 11:43:17 +0100

    About and avis pages is done

---
+++ Web/Pages/Avis/avis-info.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>avis-info</title>
+    <link rel="stylesheet" href="avis-info.css">
+  </head>
+  <body>
+    <div class="container">
+        <h1 style="text-align:center;">Titre de l'avis</h1> <br>
+        <h4>Auteur : <a href="#">Mohamed OUFRAD</a></h4> 
+        <hr width="500px"> <br>
+          <div class="discrption">
+            <p> La discrption de l'avis et le piece join :
+                Essay
+              As the corpse went past the flies left the restaurant table in a cloud
+              and rushed after it, but they came back a few minutes later.
+
+              The little crowd of mourners-all men and boys, no women--threaded
+              their way across the market-place between the piles of pomegranates
+              and the taxis and the camels, wailing a short chant over and over
+              again. What really appeals to the flies is that the corpses here
+              are never put into coffins, they are merely wrapped in a piece of
+              rag and carried on a rough wooden bier on the shoulders of four friends.
+              When the friends get to the burying-ground they hack an oblong hole a
+              foot or two deep, dump the body in it and fling over it a little of the
+              dried-up, lumpy earth, which is like broken brick. No gravestone, no
+              name, no identifying mark of any kind. The burying-ground is merely a
+              huge waste of hummocky earth, like a derelict building-lot. After a month
+              or two no one can even be certain where his own relatives are buried.
+
+              When you walk through a town like this--two hundred thousand inhabitants,
+              of whom at least twenty thousand own literally nothing except the rags
+              they stand up in--when you see how the people live, and still more how
+              easily they die, it is always difficult to believe that you are walking
+              among human beings. All colonial empires are in reality founded upon
+              that fact. The people have brown faces--besides, there are so many of
+              them! Are they really the same flesh as yourself? Do they even have
+              names? Or are they merely a kind of undifferentiated brown stuff, about
+              as individual as bees or coral insects? They rise out of the earth, they
+              sweat and starve for a few years, and then they sink back into the
+              nameless mounds of the graveyard and nobody notices that they are gone.
+              And even the graves themselves soon fade back into the soil. Sometimes,
+              out for a walk, as you break your way through the prickly pear, you
+              notice that it is rather bumpy underfoot, and only a certain regularity
+              in the bumps tells you that you are walking over skeletons.
+            </p>
+        </div>
+    </div>
+  </body>
+</html>
---
+++ Web/Pages/Avis/avis.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="avis.css">
+  </head>
+  <body>
+    <div class="container">
+      <a href="avis-info.html" class="link">
+      <div class="card mb-3" style="max-width: 18rem;">
+        <div class="card-header">Avis</div>
+        <div class="card-body">
+          <h5 class="card-title">this is the Title</h5>
+          <p class="card-text">this is the content of the Avis.</p>
+          <hr>
+          <p> the class that interested in this avis</p>
+          <p class="date">La date de l'avis</p>
+        </div>
+      </div>
+      </a>
+        <a href="avis-info.html" class="link">
+      <div class="card mb-3" style="max-width: 18rem;">
+        <div class="card-header">Avis</div>
+        <div class="card-body">
+          <h5 class="card-title">this is the Title</h5>
+          <p class="card-text">this is the content of the Avis.</p>
+          <hr>
+          <p> the class that interested in this avis</p>
+          <p class="date">La date de l'avis</p>
+        </div>
+      </div>
+      </a>
+        <a href="avis-info.html" class="link">
+    <div class="card mb-3" style="max-width: 18rem;">
+      <div class="card-header">Avis</div>
+      <div class="card-body">
+        <h5 class="card-title">this is the Title</h5>
+        <p class="card-text">this is the content of the Avis.</p>
+        <hr>
+        <p> the class that interested in this avis</p>
+        <p class="date">La date de l'avis</p>
+      </div>
+    </div>
+    </a>
+  </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
