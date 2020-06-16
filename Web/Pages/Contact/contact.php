8e260e04656d7470d1d2cd5495e9c84a5eb5f644
Samadi-AA <66368620+Samadi-AA@users.noreply.github.com>
2020-06-16 17:14:11 +0000

    update pages

--- Web/Pages/Contact/contact.php
+++ Web/Pages/Contact/contact.php
@@ -5,54 +5,42 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" >
-	<title>index</title>
+	<title>Contact us</title>
 	<link rel="stylesheet" type="text/css" href="contact.css">
-	 <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="../../fontawesome/css/all.min.css">
 
 </head>
 <body>
-<div class="container">
- <div  >
-	<form>
-
-		<h1 >  Contact Us </h1>
-	      <hr class="hr">
-		 	<input type="text" placeholder="Your Name" name="name" class="cls">
-     <br>   <br> <br>
-		 	<input type="Email"placeholder="Your Email"  name="email" class="cls">
-     <br>   <br> <br>
-		 	<input type="tel"placeholder="Your phone"  name="phone" class="cls">
-     <br>   <br>   <br>
-             <textarea  placeholder="Message" class="cl" ></textarea>
-     <br>   <br> <br>
-              <input type="submit" value="Send" name="Send" class="send">
-
-	 </form>
-</div>
-
-
-<div  class="div">
-
-	   <span class="b"><i class="fa fa-map-marker" aria-hidden="true" style='font-size:15px'></i><b>&nbsp &nbsp Adress &nbsp&nbsp&nbsp &nbsp:&nbsp&nbsp &nbsp</b> Hay El Mohammadi (Lastah)</span>
-   	<br>
-
-	<span class="b"><i class="fa fa-phone" style='font-size:15px'></i><b> Tel &nbsp &nbsp &nbsp &nbsp &nbsp&nbsp &nbsp &nbsp :&nbsp &nbsp</b>+212 5 28 55 10 10</span>
-	<br>
-	<span class="b"><i class="fa fa-fax" aria-hidden="true" style='font-size:15px'></i><b>&nbspFax &nbsp &nbsp &nbsp &nbsp&nbsp &nbsp&nbsp :&nbsp &nbsp</b>+212 5 28 55 10 20</span>
-	<br>
-	<span class="b"><i class="fa fa-envelope" aria-hidden="true" style='font-size:15px'></i><b>&nbspEmail &nbsp &nbsp &nbsp &nbsp&nbsp :&nbsp &nbsp </b>departement_fpt@gmail.com</span>
-	<br>
-	<span class="b"><i class="fa fa-facebook-square" style="font-size:15px;"></i><b>&nbspFecebook &nbsp:&nbsp &nbsp</b>&nbsp Antari Jelali</span>
-<br>
-<span class="b"><b>Site &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp :&nbsp&nbsp &nbsp </b> <a href=""> www.fpt_departement.com</a></span>
-
-</div>
-
-
-</div>
-
-
-
+	<div class="container">
+		<div class="left-box">
+			<form>
+				<h1>Contactez nous :</h1>
+				<hr>
+				<input type="text" placeholder="Your Name" name="name" class="cls">
+				<br> <br>
+				<input type="Email"placeholder="Your Email"  name="email" class="cls">
+				<br> <br>
+				<input type="tel"placeholder="Your phone"  name="phone" class="cls">
+				<br> <br>
+				<textarea  placeholder="Your message" class="cl" ></textarea>
+				<br> <br>
+				<input type="submit" value="Send" name="Send" class="send">
+			</form>
+		</div>
+		<div  class="right-box">
+			<span><i class="fa fa-map-marker-alt" aria-hidden="true"></i><b>Adress &nbsp &nbsp &nbsp : &nbsp &nbsp</b> Hay El Mouhammadi - (Lastah) - B.P: 207 - 83000 Taroudant, Maroc</span>
+			<br>
+			<span><i class="fa fa-phone"></i><b>Tel &nbsp &nbsp &nbsp : &nbsp &nbsp</b> +212 5 28 55 10 10</span>
+			<br>
+			<span><i class="fa fa-fax" aria-hidden="true"></i><b>Fax &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp: &nbsp &nbsp</b> +212 5 28 55 10 20</span>
+			<br>
+			<span><i class="fa fa-envelope" aria-hidden="true"></i><b>Email &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp: &nbsp &nbsp </b> departement_fpt@gmail.com</span>
+			<br>
+			<span><i class="fab fa-facebook-square"></i><b>Fecebook &nbsp &nbsp &nbsp : &nbsp &nbsp</b> Antari Jelali</span>
+			<br>
+			<span><i class="fas fa-globe" aria-hidden="true"></i><b>Site &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp: &nbsp &nbsp </b> <a href=""> www.dmi.ac.ma</a></span>
+		</div>
+	</div>
 </body>
 </html>
