60ca2942f1ef8b334db00846d0aa497d6331471e
Rami <corazomi88@gmail.com>
2020-06-17 11:34:52 +0000

    remove unused files

---
+++ Web/Pages/Avis/avisWidjet.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="http://localhost/Departement-math-info/Web/Pages/Avis/avis.css">
+  </head>
+  <body>
+
+    <div class="avis_container">
+      <a href="#" class="link">
+        <div class="card mb-3">
+        <div class="card-header">Avis</div>
+        <div class="card-body">
+          <h5 class="card-title">TP5 technology web</h5>
+          <!-- <p class="card-text">tp 5 technology web</p> -->
+          <hr>
+          <p><b>LP GI S4</b></p>
+          <p class="date"><b>02-05-2020</b></p>
+        </div>
+      </div>
+      </a>
+    </div>
+
+  </body>
+</html>
